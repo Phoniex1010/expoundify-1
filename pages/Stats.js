644c0fd2b85b5d42de66ad88e9467c1688a53bf6
Phoniex1010 <128490162+Phoniex1010@users.noreply.github.com>
2023-04-04 08:50:50 -0400

    test commit

--- pages/Stats.js
+++ pages/Stats.js
@@ -5,7 +5,7 @@ import React from "react";
 export default function Stats() {
     return (
         <View style={defaultStyles.container}>
-            <Text>Statistics</Text>
+            <Text>Beep</Text>
         </View>
     )
 }
